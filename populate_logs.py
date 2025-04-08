39bd3b31f68e3a1143bcff06525669511155a185
wrldofcrona <yoursocai123@gmail.com>
2025-04-08 14:43:17 +0300

    populate scripts fixing

--- populate_logs.py
+++ populate_logs.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import random
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prs_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prs_project.settings")
 
 import django
 
@@ -12,128 +12,135 @@ from collector.models import Log
 
 SEED = 0
 
-films = {'comedy':
-                 [
-                  '0475290'
-                ,'1289401'
-                ,'1292566'
-                ,'1473832'
-                ,'1489889'
-                ,'1608290'
-                ,'1679335'
-                ,'1700841'
-                ,'1711525'
-                ,'1860213'
-                ,'1878870'
-                ,'1985949'
-                ,'2005151'
-                ,'2277860'
-                ,'2387499'
-                ,'2709768'
-                ,'2823054'
-                ,'2869728'
-                ,'2937696'
-                ,'3110958'
-                ,'3381008'
-                ,'3470600'
-                ,'3521164'
-                ,'3553442'
-                ,'3783958'
-                ,'3874544'
-                ,'4034354'
-                ,'4048272'
-                ,'4136084'
-                ,'4139124'
-                ,'4438848'
-                ,'4501244'
-                ,'4513674'
-                ,'4624424'
-                ,'4651520'
-                ,'4698684'
-                ,'4901306'
-                ,'5247022'
-                ,'5512872'],
-             'drama': ['2119532'
-                ,'2543164'
-                ,'3783958'
-                ,'3315342'
-                ,'3263904'
-                ,'4034228'
-                ,'3040964'
-                ,'3741834'
-                ,'2140479'
-                ,'1179933'
-                ,'1355644'
-                ,'4550098'
-                ,'2582782'
-                ,'4975722'
-                ,'2674426'
-                ,'2005151'
-                ,'4846340'
-                ,'1860357'
-                ,'3640424'
-                ,'3553976'
-                ,'2241351'
-                ,'4052882'
-                ,'2671706'
-                ,'3774114'
-                ,'5512872'
-                ,'4172430'
-                ,'3544112'
-                ,'4513674'
-                ,'0490215'
-                ,'1619029'
-                ,'4572514'
-                ,'1878870'
-                ,'1083452'
-                ,'2025690'
-                ,'1219827'
-                ,'1972591'
-                ,'4276820'
-                ,'2381991'
-                ,'3416532'
-                ,'2547584'
-             ], 'action': [
-            '1431045', '2975590'
-            , '1386697'
-            , '3498820'
-            , '3315342'
-            , '1211837'
-            , '2948356'
-            , '3748528'
-            , '3385516'
-            , '3110958'
-            , '4196776'
-            , '4425200'
-            , '3896198'
-            , '2404435'
-            , '3731562'
-            , '1860357'
-            , '4630562'
-            , '0803096'
-            , '2660888'
-            , '3640424'
-            , '3300542'
-            , '0918940'
-            , '2094766'
-            , '5700672'
-            , '1289401'
-            , '1628841'
-            , '3393786'
-            , '4172430'
-            , '4094724'
-            , '2025690'
-            , '4116284'
-            , '3381008'
-            , '1219827'
-            , '1972591'
-            , '2381991'
-            , '2034800'
-            , '2267968'
-            , '2869728'
-            , '3949660'
-            , '3410834'
-        ,'2250912']}
+films = {
+    "comedy": [
+        "0475290",
+        "1289401",
+        "1292566",
+        "1473832",
+        "1489889",
+        "1608290",
+        "1679335",
+        "1700841",
+        "1711525",
+        "1860213",
+        "1878870",
+        "1985949",
+        "2005151",
+        "2277860",
+        "2387499",
+        "2709768",
+        "2823054",
+        "2869728",
+        "2937696",
+        "3110958",
+        "3381008",
+        "3470600",
+        "3521164",
+        "3553442",
+        "3783958",
+        "3874544",
+        "4034354",
+        "4048272",
+        "4136084",
+        "4139124",
+        "4438848",
+        "4501244",
+        "4513674",
+        "4624424",
+        "4651520",
+        "4698684",
+        "4901306",
+        "5247022",
+        "5512872",
+    ],
+    "drama": [
+        "2119532",
+        "2543164",
+        "3783958",
+        "3315342",
+        "3263904",
+        "4034228",
+        "3040964",
+        "3741834",
+        "2140479",
+        "1179933",
+        "1355644",
+        "4550098",
+        "2582782",
+        "4975722",
+        "2674426",
+        "2005151",
+        "4846340",
+        "1860357",
+        "3640424",
+        "3553976",
+        "2241351",
+        "4052882",
+        "2671706",
+        "3774114",
+        "5512872",
+        "4172430",
+        "3544112",
+        "4513674",
+        "0490215",
+        "1619029",
+        "4572514",
+        "1878870",
+        "1083452",
+        "2025690",
+        "1219827",
+        "1972591",
+        "4276820",
+        "2381991",
+        "3416532",
+        "2547584",
+    ],
+    "action": [
+        "1431045",
+        "2975590",
+        "1386697",
+        "3498820",
+        "3315342",
+        "1211837",
+        "2948356",
+        "3748528",
+        "3385516",
+        "3110958",
+        "4196776",
+        "4425200",
+        "3896198",
+        "2404435",
+        "3731562",
+        "1860357",
+        "4630562",
+        "0803096",
+        "2660888",
+        "3640424",
+        "3300542",
+        "0918940",
+        "2094766",
+        "5700672",
+        "1289401",
+        "1628841",
+        "3393786",
+        "4172430",
+        "4094724",
+        "2025690",
+        "4116284",
+        "3381008",
+        "1219827",
+        "1972591",
+        "2381991",
+        "2034800",
+        "2267968",
+        "2869728",
+        "3949660",
+        "3410834",
+        "2250912",
+    ],
+}
+
 
 class User:
     sessionId = 0
@@ -144,7 +151,7 @@ class User:
     def __init__(self, user_id, action, drama, comedy):
         self.sessionId = random.randint(0, 1000000)
         self.userId = user_id
-        self.likes = {'action': action, 'drama': drama, 'comedy': comedy}
+        self.likes = {"action": action, "drama": drama, "comedy": comedy}
         self.events = {self.sessionId: []}
 
     def get_session_id(self):
@@ -162,8 +169,8 @@ def select_film(user):
 
     genre = user.select_genre()
     interested_films = films[genre]
-    film_id = ''
-    while film_id == '':
+    film_id = ""
+    while film_id == "":
         film_candidate = interested_films[random.randint(0, len(interested_films) - 1)]
         if film_candidate not in user.events[user.sessionId]:
             film_id = film_candidate
@@ -172,7 +179,13 @@ def select_film(user):
 
 
 def select_action(user):
-    actions = {'genreView': 15, 'details': 50, 'moreDetails': 24, 'addToList': 10, 'buy': 1}
+    actions = {
+        "genreView": 15,
+        "details": 50,
+        "moreDetails": 24,
+        "addToList": 10,
+        "buy": 1,
+    }
 
     return sample(actions)
 
@@ -209,16 +222,24 @@ def main():
         user = users[randomuser_id]
         selected_film = select_film(user)
         action = select_action(user)
-        if action == 'buy':
+        if action == "buy":
             user.events[user.sessionId].append(selected_film)
-        print("user id " + str(user.userId) + " selects film " + str(selected_film) + " and " + action)
+        print(
+            "user id "
+            + str(user.userId)
+            + " selects film "
+            + str(selected_film)
+            + " and "
+            + action
+        )
 
-        l = Log(user_id=str(user.userId),
-                content_id=selected_film,
-                event=action,
-                session_id=str(user.get_session_id()),
-                created=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                )
+        l = Log(
+            user_id=str(user.userId),
+            content_id=selected_film,
+            event=action,
+            session_id=str(user.get_session_id()),
+            created=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        )
         l.save()
 
     print("users\n")
@@ -229,6 +250,6 @@ def main():
                 print(" {}: {}".format(key, value))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Starting MovieGeeks Log Population script...")
     main()
--- populate_ratings_from_MovieLens_data.py
+++ populate_ratings_from_MovieLens_data.py
@@ -2,8 +2,7 @@ import os
 import urllib.request
 
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prs_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prs_project.settings")
 
 import django
 import csv, decimal, datetime
@@ -12,39 +11,52 @@ django.setup()
 
 from analytics.models import Rating
 
+
 def create_rating(user_id, content_id, rating, timestamp):
 
-    rating = Rating(user_id=user_id, movie_id=content_id, rating=decimal.Decimal(rating),
-                    rating_timestamp=datetime.datetime.fromtimestamp(float(timestamp)))
+    rating = Rating(
+        user_id=user_id,
+        movie_id=content_id,
+        rating=decimal.Decimal(rating),
+        rating_timestamp=datetime.datetime.fromtimestamp(float(timestamp)),
+    )
 
     return rating
 
 
 def delete_db():
-    print('truncate db')
+    print("truncate db")
     Rating.objects.all().delete()
-    print('finished truncate db')
+    print("finished truncate db")
+
 
 def populate(path):
 
-    with open(path, newline='') as csvfile:
+    with open(path, newline="") as csvfile:
         rating_reader = csv.DictReader(csvfile, skipinitialspace=True)
         ratings = []
         for rating in rating_reader:
             if len(rating) == 4:
-                rating = create_rating(rating['userId'], rating['movieId'], rating['rating'], rating['timestamp'])
+                rating = create_rating(
+                    rating["userId"],
+                    rating["movieId"],
+                    rating["rating"],
+                    rating["timestamp"],
+                )
                 ratings.append(rating)
             if len(ratings) == 1000:
                 Rating.objects.bulk_create(ratings)
                 ratings = []
                 print(".")
         Rating.objects.bulk_create(ratings)
-    print('database is populated with {} ratings'.format(Rating.objects.count()))
+    print("database is populated with {} ratings".format(Rating.objects.count()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("This script will load the MovieLens latest dataset - only the ratings")
-    print("Beware that this dataset doesnt work with the moviegeek website. run populate_ratings.py for that")
+    print(
+        "Beware that this dataset doesnt work with the moviegeek website. run populate_ratings.py for that"
+    )
 
     print("Before starting this you need to download the latest from here: ")
     print("http://grouplens.org/datasets/movielens/latest/")
@@ -53,4 +65,4 @@ if __name__ == '__main__':
     print("Usage: python populate_ratings_from_MovieLens_data.py ")
     print("Starting MovieGeeks Population script...")
     delete_db()
-    populate('/Users/u0157492/Downloads/ml-latest-small/ratings.csv')
+    populate("/Users/u0157492/Downloads/ml-latest-small/ratings.csv")
