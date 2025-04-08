77cb3f15a3686bb17bd4ac4439689d2aee74eb27
wrldofcrona <yoursocai123@gmail.com>
2025-04-08 15:04:10 +0300

    fix urls

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,17 +1,29 @@
-from django.urls import path, re_path
+from django.urls import re_path
 
 from recommender import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('user/<int:user_id>/', views.user, name='user'),
-    path('content/<int:content_id>/', views.content, name='content'),
-    path('cluster/<int:cluster_id>/', views.cluster, name='cluster'),
-    re_path(r'^api/get_statistics', views.get_statistics, name='get statistics'),
-    re_path(r'^api/events_on_conversions', views.events_on_conversions, name='events_on_conversions'),
-    re_path(r'^api/ratings_distribution', views.ratings_distribution, name='ratings_distribution'),
-    re_path(r'^api/top_content', views.top_content, name='top_content'),
-    re_path(r'^api/clusters', views.clusters, name='clusters'),
-    re_path(r'^lda', views.lda, name='lda'),
-    re_path(r'^similarity', views.similarity_graph, name='similarity_graph'),
+    re_path(r'^chart/', views.chart, name='chart'),
+    re_path(r'^association_rule/(?P<content_id>\w+)/$',
+        views.get_association_rules_for,
+        name='get_association_rules_for'),
+    re_path(r'^ar/(?P<user_id>\w+)/$',
+        views.recs_using_association_rules,
+        name='recs_using_association_rules'),
+    re_path(r'^sim/user/(?P<user_id>\w+)/(?P<sim_method>\w+)/$',
+        views.similar_users, name='similar_users'),
+    re_path(r'^cb/item/(?P<content_id>\w+)/$',
+        views.similar_content, name='similar_content'),
+    re_path(r'^cb/user/(?P<user_id>\w+)/$',
+        views.recs_cb, name='recs_cb'),
+    re_path(r'^cf/user/(?P<user_id>\w+)/$',
+        views.recs_cf, name='recs_cb'),
+    re_path(r'^funk/user/(?P<user_id>\w+)/$',
+        views.recs_funksvd, name='recs_funksvd'),
+    re_path(r'^fwls/user/(?P<user_id>\w+)/$',
+        views.recs_fwls, name='recs_fwls'),
+    re_path(r'^bpr/user/(?P<user_id>\w+)/$',
+        views.recs_bpr, name='recs_fwls'),
+    re_path(r'^pop/user/(?P<user_id>\w+)/$',
+        views.recs_pop, name='recs_pop')
 ]
